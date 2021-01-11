4992d77aa41d9e4c3104dd458f4fcf5c2a831f61
Evan <evanfujita@gmail.com>
2021-01-11 17:43:02 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    status = 200
+
+    get '/name' do
+        "My name is Evan"
+    end
+
+    get '/hometown' do
+        "My hometown is Chicago"
+    end
+
+
+    get '/favorite-song' do
+        "My favorite song is Art of Almost"
+    end
+
 end
